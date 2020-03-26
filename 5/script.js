7291cc14469e7ab7c17050d8f411b365bbe89c96
Vasya <example@gmail.com>
2020-03-26 13:34:04 +0200

    5 dz

---
+++ 5/script.js
@@ -0,0 +1,25 @@
+// let x = 5;
+// alert(x++);
+
+// console.log([] + false - null + true);
+
+
+
+// alert('1'[0])
+
+// console.log(2 && 1 && null && 0 && undefined);
+// alert( +"Infinity" );
+// alert( null || 2 && 3 || 4 );
+
+// let a = [1, 2, 3];
+// let b = [1, 2, 3];
+
+// if (a == b) {
+//     console.log(true);
+    
+// } else {
+//     console.log(false);
+    
+// }
+
+console.log(0 || "" || 2 || undefined || true || falsе);
